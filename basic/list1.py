bb870f771b0c12c4607e3815ec9b622a91adf461
Zhengli Liao <zhengli.liao@gmail.com>
2013-01-06 17:11:02 +0800

    Completed a new method, match_ends()
    Passed all test cases.

--- basic/list1.py
+++ basic/list1.py
@@ -21,9 +21,11 @@
 # and last chars of the string are the same.
 # Note: python does not have a ++ operator, but += works.
 def match_ends(words):
-  # +++your code here+++
-  return
-
+  i=0
+  for s in words:
+    if len(s)>= 2 and s[0] == s[-1]:
+      i+=1
+  return i
 
 # B. front_x
 # Given a list of strings, return a list with the strings
